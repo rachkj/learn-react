e1c8ee5dbc377315120f772de619e0cb6ef72c55
Rachana Kallada Jayaraj <kjrach@gmail.com>
2024-02-12 19:29:34 -0500

    added code

--- src/pages/qcomps/list_keys_id.js
+++ src/pages/qcomps/list_keys_id.js
@@ -15,10 +15,12 @@ export const people = [{
 function getImageUrl(imageId) {
     return "https://i.imgur.com/" + imageId + "s.jpg"
 }
+
 export default function List() {
-  const listItems = people.map(person =>
-    <li>
+  const listItems = people.map(person => (
+    <li key={person.id}>
+      <img src={getImageUrl(person.imageId)} alt={person.name} />
     </li>
-  );
+  ));
   return <ul>{listItems}</ul>;
 }
