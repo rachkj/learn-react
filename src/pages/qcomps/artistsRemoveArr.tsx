4de5561392ceafbde33de1ecbc3a88b20b594c2d
Rachana Kallada Jayaraj <kjrach@gmail.com>
2025-02-02 09:37:20 +0530

    React state assignment

--- src/pages/qcomps/artistsRemoveArr.tsx
+++ src/pages/qcomps/artistsRemoveArr.tsx
@@ -1,28 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 let initialArtists = [
-  { id: 0, name: 'Marta Colvin Andrade' },
-  { id: 1, name: 'Lamidi Olonade Fakeye'},
-  { id: 2, name: 'Louise Nevelson'},
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
 ];
 
 export default function List() {
-  const [artists, setArtists] = useState(
-    initialArtists
-  );
+  const [artists, setArtists] = useState(initialArtists);
+
+  function handleDelete(artistId: number) {
+    // Create a new array without mutating the original
+    setArtists(artists.filter((artist) => artist.id !== artistId));
+  }
 
   return (
     <>
       <h1>Inspiring sculptors:</h1>
       <ul>
-        {artists.map(artist => (
+        {artists.map((artist) => (
           <li key={artist.id}>
-            {artist.name}{' '}
-            <button onClick={() => {
-              artists.splice(artist.id, 1)
-            }}>
-              Delete
-            </button>
+            {artist.name}{" "}
+            <button onClick={() => handleDelete(artist.id)}>Delete</button>
           </li>
         ))}
       </ul>
